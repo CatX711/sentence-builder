3372e16c5934ea5d4fdc1be62237969a2f5fbe2c
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-05-29 21:23:37 +0100

    uploaded code

---
+++ sentence builder.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+print("Sentence 1:")
+
+print("Enter a adjective:")
+adjective = input()
+print("Enter a noun:")
+noun = input()
+print("Enter a noun:")
+noun2 = input()
+print("Enter an adverb:")
+adverb = input()
+print("Enter a verb:")
+verb = input()
+print("Enter a noun:")
+noun3 = input()
+
+print("The ", adjective, noun, "walked headfirst into a", noun2, "which", adverb, verb, "the ", noun, "into a", noun3 )
